d7a870eaeb606be003dd9607f720be352f15b024
NaveenPrabodha <naveenprabodha3@gmail.com>
2026-01-26 21:42:22 +0530

    finalized db connection

--- app/db.py
+++ app/db.py
@@ -2,13 +2,17 @@ from collections.abc import AsyncGenerator
 import uuid
 
 from sqlalchemy import Column, String, Text, DateTime, ForeignKey
-from sqlalchemy.dialects.postgresql import uuid
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
+from datetime import datetime
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
-class Post(DeclarativeBase):
+class Base(DeclarativeBase):
+    pass
+
+class Post(Base):
     __tablename__ = "posts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
@@ -27,6 +31,8 @@ async_session_maker = async_sessionmaker(
 
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(DeclarativeBase.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
-async def get_async_session() -> AsyncGenerator[AsyncSession, None]
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
